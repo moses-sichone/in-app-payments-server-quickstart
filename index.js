c7e0366b5cf8657c3d611e077f15008991035d41
msich <m.sichone21@gmail.com>
2024-02-21 16:08:16 +0200

    updated checkout price & quantity & name 0.1

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.post('/chargeForCookie', async (request, response) => {
   const requestBody = request.body;
   try {
     const locationId =  process.env.LOCATION_ID;
+    console.log(requestBody.nonce);
+    console.log(requestBody.amount);
+    console.log(requestBody.name);
     const createOrderRequest = getOrderRequest(locationId, requestBody.amount, requestBody.quantity, requestBody.name);
     const createOrderResponse = await ordersApi.createOrder(createOrderRequest);
 
